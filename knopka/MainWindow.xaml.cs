f6453c24e8fd2e66d47bf1ed209137888cb1058f
Elair <Elair@LAPTOP-0U63LG0N>
2024-03-02 22:08:20 +0500

    Добавьте файлы проекта.

---
+++ knopka/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace knopka
+{
+  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Button button = new Button();
+            button.Content = "Новая кнопка";
+            button.Width = 100;
+            button.Height = 30;
+            Canvas.SetLeft(button, e.GetPosition(this).X);
+            Canvas.SetTop(button, e.GetPosition(this).Y);
+
+            canvas.Children.Add(button);
+
+        }
+    }
+}
